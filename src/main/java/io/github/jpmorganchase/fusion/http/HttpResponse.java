5c5e2dffba71e53dc893a1f5faaf5940389ed9a3
martinmcclintock <44686356+martinmcclintock@users.noreply.github.com>
2023-03-16 13:41:27 +0000

    Incremental Fixes (#6)
    * Initial refactoring changes proposed for Fusion Java SDK
    
    * Updated POM to prep for Maven central requirments
    
    * Changing group ID to align for Maven central
    
    * Incremental test improvement.
    Switch PIT to run during regular builds all the time with a threshold of 85% mutation coverage for now
    
    * Ran Spotless to fix style errores

--- src/main/java/io/github/jpmorganchase/fusion/http/HttpResponse.java
+++ src/main/java/io/github/jpmorganchase/fusion/http/HttpResponse.java
@@ -12,4 +12,8 @@ public class HttpResponse<T> {
     int statusCode;
     Map<String, List<String>> headers;
     T body;
+
+    public boolean isError() {
+        return statusCode >= 400;
+    }
 }
