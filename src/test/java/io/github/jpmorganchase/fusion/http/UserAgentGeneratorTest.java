252c581665843188f465c54bbd1278dd65da9a04
Martin McClintock <martin.mcclintock@gmail.com>
2023-03-27 18:59:03 +0100

    Enabling test for user agent generation

--- src/test/java/io/github/jpmorganchase/fusion/http/UserAgentGeneratorTest.java
+++ src/test/java/io/github/jpmorganchase/fusion/http/UserAgentGeneratorTest.java
@@ -4,16 +4,14 @@ import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class UserAgentGeneratorTest {
 
     @Test
-    @Disabled(
-            "This needs work since in it's current implementation the value depends entirely on the actual runtime of the test")
     void testMyUserAgent() {
         String agent = UserAgentGenerator.getUserAgentString(JdkClient.class);
-        assertThat(agent, is(equalTo("fusion-java-sdk/0.0.0 (JdkClient) Java/1.8.0_362")));
+        String javaVersion = System.getProperty("java.version");
+        assertThat(agent, is(equalTo("fusion-java-sdk/UNKNOWN (JdkClient) Java/" + javaVersion)));
     }
 }
