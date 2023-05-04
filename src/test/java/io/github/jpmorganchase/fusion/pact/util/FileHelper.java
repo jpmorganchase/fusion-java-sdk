4fb2319b0de9bf035340507e359e0e1948ef83cf
Ian Knight <128476114+knighto82@users.noreply.github.com>
2023-05-04 15:05:19 +0100

    spotless ....

--- src/test/java/io/github/jpmorganchase/fusion/pact/util/FileHelper.java
+++ src/test/java/io/github/jpmorganchase/fusion/pact/util/FileHelper.java
@@ -3,17 +3,15 @@ package io.github.jpmorganchase.fusion.pact.util;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-import lombok.SneakyThrows;
-
 public class FileHelper {
 
     private FileHelper() {}
 
     /**
-     This is a test method to read contents from an input stream and return them as a string.
-
-     @param is The input stream to read from.
-     @return The contents of the input stream as a string.
+     * This is a test method to read contents from an input stream and return them as a string.
+     *
+     * @param is The input stream to read from.
+     * @return The contents of the input stream as a string.
      */
     public static String readContentsFromStream(InputStream is) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
