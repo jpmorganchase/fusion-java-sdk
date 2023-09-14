042730fd98f2658bbc45240ea4b6634c6f492b24
Ian Knight <128476114+knighto82@users.noreply.github.com>
2023-09-14 16:46:30 +0100

    Enhanced exception handling (#49)
    * Corrected Issue with Null Pointer Returned when error stream is null
    
    * Enhanced error details returned when a 400 is encountered.

---
+++ src/test/java/io/github/jpmorganchase/fusion/api/tools/RegexBasedErrorParserTest.java
@@ -0,0 +1,38 @@
+package io.github.jpmorganchase.fusion.api.tools;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegexBasedErrorParserTest {
+
+    @Test
+    public void testJsonWithError() {
+        String json = "{\"error\": \"blah\"}";
+        String actual = RegexBasedErrorParser.get(json);
+        assertThat(actual, equalTo("blah"));
+    }
+
+    @Test
+    public void testJsonWithErrorContainingSpaces() {
+        String json = "{\"error\" : \"blah\"}";
+        String actual = RegexBasedErrorParser.get(json);
+        assertThat(actual, equalTo("blah"));
+    }
+
+    @Test
+    public void testJsonWithoutError() {
+        String json = "{\"detail\": \"blah\"}";
+        String actual = RegexBasedErrorParser.get(json);
+        assertThat(actual, equalTo("Unknown"));
+    }
+
+    @Test
+    public void testJsonWithoutErrorButWithErrorInSentence() {
+        String json = "{\"detail\": \"there is an error\"}";
+        String actual = RegexBasedErrorParser.get(json);
+        assertThat(actual, equalTo("Unknown"));
+    }
+}
