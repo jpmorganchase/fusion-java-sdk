d8b4f02a76023aa93d4eeed601e4bd27ed66f9bc
Ian Knight <128476114+knighto82@users.noreply.github.com>
2024-11-21 12:23:00 +0000

    Updated DatasetBuilder tests.

--- src/test/java/io/github/jpmorganchase/fusion/model/DatasetBuilderTest.java
+++ src/test/java/io/github/jpmorganchase/fusion/model/DatasetBuilderTest.java
@@ -4,14 +4,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import io.github.jpmorganchase.fusion.api.APIManager;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class DatasetBuilderTest {
 
     @Test
     void constructionWithBuilderCorrectlyPopulatesAllFields() {
+        APIManager apiManager = Mockito.mock(APIManager.class);
         Map<String, Object> varArgs = new HashMap<>();
         varArgs.put("key1", "value1");
         Dataset d = Dataset.builder()
@@ -21,6 +24,9 @@ public class DatasetBuilderTest {
                 .linkedEntity("The entity")
                 .title("The title")
                 .frequency("The frequency")
+                .rootUrl("http://foobar/api/v1")
+                .catalogIdentifier("foobar")
+                .apiManager(apiManager)
                 .build();
 
         assertThat(d.getIdentifier(), is(equalTo("The identifier")));
@@ -29,5 +35,17 @@ public class DatasetBuilderTest {
         assertThat(d.getLinkedEntity(), is(equalTo("The entity")));
         assertThat(d.getTitle(), is(equalTo("The title")));
         assertThat(d.getFrequency(), is(equalTo("The frequency")));
+        assertThat(d.getRootUrl(), is(equalTo("http://foobar/api/v1")));
+        assertThat(d.getCatalogIdentifier(), is(equalTo("foobar")));
+        assertThat(d.getApiManager(), is(equalTo(apiManager)));
+    }
+
+    @Test
+    void constructionWithBuilderCorrectlyPopulatesAllFieldsWhenSingleVarArgAdded() {
+        Map<String, Object> varArgs = new HashMap<>();
+        varArgs.put("key1", "value1");
+        Dataset d = Dataset.builder().varArg("key1", "value1").build();
+
+        assertThat(d.getVarArgs(), is(equalTo(varArgs)));
     }
 }
