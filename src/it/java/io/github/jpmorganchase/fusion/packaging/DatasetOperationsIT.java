97ace62e64cd5fef6d8fa049265c3b9393fcb887
Ian Knight <128476114+knighto82@users.noreply.github.com>
2024-11-21 13:45:18 +0000

    Added additional Integration Tests to support the listing of datasets.

--- src/it/java/io/github/jpmorganchase/fusion/packaging/DatasetOperationsIT.java
+++ src/it/java/io/github/jpmorganchase/fusion/packaging/DatasetOperationsIT.java
@@ -7,6 +7,8 @@ import io.github.jpmorganchase.fusion.Fusion;
 import io.github.jpmorganchase.fusion.FusionConfiguration;
 import io.github.jpmorganchase.fusion.model.Dataset;
 import io.github.jpmorganchase.fusion.test.TestUtils;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,9 +20,12 @@ import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static io.github.jpmorganchase.fusion.test.TestUtils.listOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 @ExtendWith(WireMockExtension.class)
 public class DatasetOperationsIT {
@@ -49,11 +54,11 @@ public class DatasetOperationsIT {
     public void testCreateDataset() {
         // Given
         wireMockRule.stubFor(WireMock.post(WireMock.urlEqualTo("/catalogs/common/datasets/SD0001"))
-                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset_SD0001_create_request.json")))
+                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset-SD0001-create-request.json")))
                 .willReturn(WireMock.aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(200)
-                        .withBody("dataset/dataset_create_response.json")));
+                        .withBodyFile("dataset/dataset-create-response.json")));
 
         Dataset dataset = sdk.builders().dataset()
                 .identifier("SD0001")
@@ -67,38 +72,39 @@ public class DatasetOperationsIT {
         dataset.create();
 
         // Then Verify the response
+        //TODO :: Contract for response of dataset.create() needs to be decided
     }
 
     @Test
     public void testCreateDatasetWithVarArgs() {
         // Given
         wireMockRule.stubFor(WireMock.post(WireMock.urlEqualTo("/catalogs/common/datasets/SD0002"))
-                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset_SD0002_create_request.json")))
+                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset-SD0002-create-request.json")))
                 .willReturn(WireMock.aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(200)
-                        .withBody("dataset/dataset_create_response.json")));
+                        .withBodyFile("dataset/dataset-create-response.json")));
 
         Dataset dataset = sdk.builders().dataset()
                 .identifier("SD0002")
-                .description("Sample dataset description 1")
+                .description("Sample dataset description 2")
                 .linkedEntity("SD0002/")
-                .title("Sample Dataset 1 | North America")
+                .title("Sample Dataset 2 | North America")
                 .frequency("Daily")
-                .varArg("category", listOf("Category 1"))
-                .varArg("createdDate", "2022-02-05")
-                .varArg("coverageStartDate", "2022-02-05")
-                .varArg("coverageEndDate", "2023-03-08")
+                .varArg("category", listOf("Category 2"))
+                .varArg("createdDate", "2022-02-06")
+                .varArg("coverageStartDate", "2022-02-06")
+                .varArg("coverageEndDate", "2023-03-09")
                 .varArg("isThirdPartyData", Boolean.FALSE)
                 .varArg("isInternalOnlyDataset", Boolean.FALSE)
                 .varArg("language", "English")
-                .varArg("maintainer", "Maintainer 1")
-                .varArg("modifiedDate", "2023-03-08")
-                .varArg("publisher", "Publisher 1")
+                .varArg("maintainer", "Maintainer 2")
+                .varArg("modifiedDate", "2023-03-09")
+                .varArg("publisher", "Publisher 2")
                 .varArg("region", listOf("North America"))
-                .varArg("source", listOf("Source System 1"))
-                .varArg("subCategory", listOf("Subcategory 1"))
-                .varArg("tag", listOf("Tag1"))
+                .varArg("source", listOf("Source System 2"))
+                .varArg("subCategory", listOf("Subcategory 2"))
+                .varArg("tag", listOf("Tag2"))
                 .varArg("isRestricted", Boolean.FALSE)
                 .varArg("isRawData", Boolean.FALSE)
                 .varArg("hasSample", Boolean.FALSE)
@@ -109,17 +115,18 @@ public class DatasetOperationsIT {
 
 
         // Then Verify the response
+        //TODO :: Contract for response of dataset.create() needs to be decided
     }
 
     @Test
     public void testCreateDatasetOverrideDefaultCatalog() {
         // Given
         wireMockRule.stubFor(WireMock.post(WireMock.urlEqualTo("/catalogs/foobar/datasets/SD0001"))
-                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset_SD0001_create_request.json")))
+                .withRequestBody(equalToJson(TestUtils.loadJsonForIt("dataset/dataset-SD0001-create-request.json")))
                 .willReturn(WireMock.aResponse()
                         .withHeader("Content-Type", "application/json")
                         .withStatus(200)
-                        .withBody("dataset/dataset_create_response.json")));
+                        .withBodyFile("dataset/dataset-create-response.json")));
 
         Dataset dataset = sdk.builders().dataset()
                 .identifier("SD0001")
@@ -134,6 +141,45 @@ public class DatasetOperationsIT {
         dataset.create();
 
         // Then Verify the response
+        //TODO :: Contract for response of dataset.create() needs to be decided
+    }
+
+    @Test
+    public void testListDatasets() {
+        // Given
+        wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/catalogs/common/datasets"))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withStatus(200)
+                        .withBodyFile("dataset/multiple-dataset-response.json")));
+
+        // When
+        Map<String, Dataset> datasets = sdk.listDatasets();
+
+        // Then Verify the response
+        MatcherAssert.assertThat(datasets.containsKey("SD0001"), is(equalTo(true)));
+        MatcherAssert.assertThat(datasets.containsKey("SD0002"), is(equalTo(true)));
+        MatcherAssert.assertThat(datasets.containsKey("SD0003"), is(equalTo(true)));
+
+    }
+
+    @Test
+    public void testListDatasetsUsingIdContains() {
+        // Given
+        wireMockRule.stubFor(WireMock.get(WireMock.urlEqualTo("/catalogs/common/datasets"))
+                .willReturn(WireMock.aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withStatus(200)
+                        .withBodyFile("dataset/multiple-dataset-response.json")));
+
+        // When
+        Map<String, Dataset> datasets = sdk.listDatasets("common", "SD0001", true);
+
+        // Then Verify the response
+        MatcherAssert.assertThat(datasets.containsKey("SD0001"), is(equalTo(true)));
+        MatcherAssert.assertThat(datasets.containsKey("SD0002"), is(equalTo(false)));
+        MatcherAssert.assertThat(datasets.containsKey("SD0003"), is(equalTo(false)));
+
     }
 
 }
