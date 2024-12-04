38266e795237c20331ea8aa584052d9bd4bfcc5d
P Karthik <pkarthik1901@gmail.com>
2024-12-04 20:11:01 +0530

    added a few test cases for Flow in DatasetBuilderTest

--- src/test/java/io/github/jpmorganchase/fusion/model/DatasetBuilderTest.java
+++ src/test/java/io/github/jpmorganchase/fusion/model/DatasetBuilderTest.java
@@ -45,6 +45,76 @@ public class DatasetBuilderTest {
     }
 
     @Test
+    void constructionWithBuilderCorrectlyPopulatesForTypeFlowInput() {
+        APIManager apiManager = Mockito.mock(APIManager.class);
+        Application producerApplicationId = Application.builder().sealId("123456").build();
+        Application[] consumerApplicationId = new Application[] {Application.builder().sealId("456789").build()};
+        Map<String, Object> varArgs = new HashMap<>();
+        varArgs.put("key1", "value1");
+        Dataset d = Dataset.builder()
+                .identifier("The identifier")
+                .varArgs(varArgs)
+                .description("The description")
+                .linkedEntity("The entity")
+                .title("The title")
+                .frequency("The frequency")
+                .inputFlow(producerApplicationId, consumerApplicationId)
+                .rootUrl("http://foobar/api/v1/")
+                .catalogIdentifier("foobar")
+                .apiManager(apiManager)
+                .build();
+
+        assertThat(d.getIdentifier(), is(equalTo("The identifier")));
+        assertThat(d.getVarArgs(), is(equalTo(varArgs)));
+        assertThat(d.getDescription(), is(equalTo("The description")));
+        assertThat(d.getLinkedEntity(), is(equalTo("The entity")));
+        assertThat(d.getTitle(), is(equalTo("The title")));
+        assertThat(d.getFrequency(), is(equalTo("The frequency")));
+        assertThat(d.getType(), is(equalTo("Flow")));
+        assertThat(d.getFlowDetails().getFlowDirection(), is(equalTo("Input")));
+        assertThat(d.getProducerApplicationId(), is(equalTo(producerApplicationId)));
+        assertThat(d.getConsumerApplicationId(), is(equalTo(consumerApplicationId)));
+        assertThat(d.getRootUrl(), is(equalTo("http://foobar/api/v1/")));
+        assertThat(d.getCatalogIdentifier(), is(equalTo("foobar")));
+        assertThat(d.getApiManager(), is(equalTo(apiManager)));
+    }
+
+    @Test
+    void constructionWithBuilderCorrectlyPopulatesForTypeFlowOutput() {
+        APIManager apiManager = Mockito.mock(APIManager.class);
+        Application producerApplicationId = Application.builder().sealId("123456").build();
+        Application[] consumerApplicationId = new Application[] {Application.builder().sealId("456789").build()};
+        Map<String, Object> varArgs = new HashMap<>();
+        varArgs.put("key1", "value1");
+        Dataset d = Dataset.builder()
+                .identifier("The identifier")
+                .varArgs(varArgs)
+                .description("The description")
+                .linkedEntity("The entity")
+                .title("The title")
+                .frequency("The frequency")
+                .outputFlow(producerApplicationId, consumerApplicationId)
+                .rootUrl("http://foobar/api/v1/")
+                .catalogIdentifier("foobar")
+                .apiManager(apiManager)
+                .build();
+
+        assertThat(d.getIdentifier(), is(equalTo("The identifier")));
+        assertThat(d.getVarArgs(), is(equalTo(varArgs)));
+        assertThat(d.getDescription(), is(equalTo("The description")));
+        assertThat(d.getLinkedEntity(), is(equalTo("The entity")));
+        assertThat(d.getTitle(), is(equalTo("The title")));
+        assertThat(d.getFrequency(), is(equalTo("The frequency")));
+        assertThat(d.getType(), is(equalTo("Flow")));
+        assertThat(d.getFlowDetails().getFlowDirection(), is(equalTo("Output")));
+        assertThat(d.getProducerApplicationId(), is(equalTo(producerApplicationId)));
+        assertThat(d.getConsumerApplicationId(), is(equalTo(consumerApplicationId)));
+        assertThat(d.getRootUrl(), is(equalTo("http://foobar/api/v1/")));
+        assertThat(d.getCatalogIdentifier(), is(equalTo("foobar")));
+        assertThat(d.getApiManager(), is(equalTo(apiManager)));
+    }
+
+    @Test
     void constructionWithBuilderCorrectlyPopulatesAllFieldsWhenSingleVarArgAdded() {
         Map<String, Object> varArgs = new HashMap<>();
         varArgs.put("key1", "value1");
