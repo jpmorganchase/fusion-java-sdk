2516d494d68e94a0bfdfad2ef1a12d2dd1452df5
P Karthik <pkarthik1901@gmail.com>
2024-12-05 22:14:00 +0530

    added test case in FlowTest to prove ability to consume array of consumerApplicationId

--- src/test/java/io/github/jpmorganchase/fusion/model/FlowTest.java
+++ src/test/java/io/github/jpmorganchase/fusion/model/FlowTest.java
@@ -10,7 +10,15 @@ public class FlowTest {
 
     @Test
     void constructionWithBuilderCorrectlyPopulatesAllFields() {
-        Flow flow = Flow.builder().flowDirection("The direction").build();
+        Flow flow = Flow.builder()
+                .flowDirection("The direction")
+                .producerApplicationId(Application.builder().sealId("123456").build())
+                .consumerApplicationId(Application.builder().sealId("456789").build())
+                .consumerApplicationId(Application.builder().sealId("901234").build())
+                .build();
         assertThat(flow.getFlowDirection(), is(equalTo("The direction")));
+        assertThat(flow.getProducerApplicationId().getId(), is(equalTo("123456")));
+        assertThat(flow.getConsumerApplicationId().get(0).getId(), is(equalTo("456789")));
+        assertThat(flow.getConsumerApplicationId().get(1).getId(), is(equalTo("901234")));
     }
 }
